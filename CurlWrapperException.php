9268faceb4fc9da57e099dba8601c26a1332f253
metaer <pavelpopovw@gmail.com>
2017-11-24 03:58:11 +0300

    Initial commit

---
+++ CurlWrapperException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavel
+ * Date: 19.10.17
+ * Time: 19:43
+ */
+
+namespace Metaer\CurlWrapperBundle;
+
+
+class CurlWrapperException extends \Exception
+{
+
+}
---
+++ MetaerCurlWrapperBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Metaer\CurlWrapperBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MetaerCurlWrapperBundle extends Bundle
+{
+}
