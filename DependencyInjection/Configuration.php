18d568a57f25c45b9ac353f03493ccd111ced80b
metaer <pavelpopovw@gmail.com>
2018-01-11 19:21:48 +0300

    required classπ

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,6 +20,10 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('metaer_curl_wrapper');
 
+        $rootNode
+            ->children()
+                ->scalarNode('main_class')->isRequired()->end();
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
